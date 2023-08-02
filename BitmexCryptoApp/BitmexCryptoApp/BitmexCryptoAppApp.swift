b1233b86ec223848fd192661c62e4dc39ab7211e
“Alireza” <alireza.sotoodeh.1@gmail.com>
2023-08-02 17:26:21 +0330

    Make initial xcode project

---
+++ BitmexCryptoApp/BitmexCryptoApp/BitmexCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitmexCryptoAppApp.swift
+//  BitmexCryptoApp
+//
+//  Created by Alireza Sotoudeh on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BitmexCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
